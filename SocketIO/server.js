639390b025cb90c8852de1771c586c02efb3dca1
tanzeel-khan1 <tanzeel0680@gmail.com>
2026-05-21 11:43:59 +0500

    socket

--- SocketIO/server.js
+++ SocketIO/server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 // 🔹 Socket.io init
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:4001",
+    origin: "https://chat-steel-eta.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
